548c17a2bfdaf0200d3bbd4d7112f74ecc09300a
Michael Brose <39873526+mwb77@users.noreply.github.com>
2025-10-02 14:11:12 +0000

    feat: update backend

--- backend/src/services/posts.js
+++ backend/src/services/posts.js
@@ -9,7 +9,7 @@ async function listPosts(
   query = {},
   { sortBy = 'createdAt', sortOrder = 'descending' } = {},
 ) {
-  return (await Post.find(query)).toSorted({ [sortBy]: sortOrder })
+  return await Post.find(query).sort({ [sortBy]: sortOrder })
 }
 
 export async function listAllPosts(options) {
@@ -22,4 +22,20 @@ export async function listPostsByAuthor(author, options) {
 
 export async function listPostsByTag(tags, options) {
   return await listPosts({ tags }, options)
+}
+
+export async function getPostById(postId) {
+  return await Post.findById(postId)
+}
+
+export async function updatePost(postId, { title, author, contents, tags }) {
+  return await Post.findOneAndUpdate(
+    { _id: postId },
+    { $set: { title, author, contents, tags } },
+    { new: true },
+  )
+}
+
+export async function deletePost(postId) {
+  return await Post.deleteOne({ _id: postId })
 }
