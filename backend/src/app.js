ebbc47315fbfb19529951dbcc86999e572b0cfff
Michael Brose <39873526+mwb77@users.noreply.github.com>
2025-10-06 18:31:32 +0000

    build: get

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,8 @@
 import express from 'express'
+import { postsRoutes } from './routes/posts.js'
 const app = express()
+postsRoutes(app)
 app.get('/', (reg, res) => {
-  res.send('Hello from Express APPPPP!')
+  res.send('Hello from Express!')
 })
 export { app }
