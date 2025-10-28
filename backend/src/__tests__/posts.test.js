a0e006a2b02d726cd238a52b5648ff2dd09800a0
Michael Brose <39873526+mwb77@users.noreply.github.com>
2025-10-28 13:10:40 +0000

    feat: module 6, user auth

--- backend/src/__tests__/posts.test.js
+++ backend/src/__tests__/posts.test.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose'
-import { describe, expect, test, beforeEach } from '@jest/globals'
+import { describe, expect, test, beforeEach, beforeAll } from '@jest/globals'
 
 import {
   createPost,
@@ -12,16 +12,23 @@ import {
 } from '../services/posts.js'
 import { Post } from '../db/models/post.js'
 
-const samplePosts = [
-  { title: 'Learning Redux', author: 'Daniel Bugl', tags: ['redux'] },
-  { title: 'Learn React Hooks', author: 'Daniel Bugl', tags: ['react'] },
-  {
-    title: 'Full-Stack React Projects',
-    author: 'Daniel Bugl',
-    tags: ['react', 'nodejs'],
-  },
-  { title: 'Guide to TypeScript' },
-]
+import { createUser } from '../services/users.js'
+
+let testUser = null
+let samplePosts = []
+
+beforeAll(async () => {
+  testUser = await createUser({ username: 'sample', password: 'user' })
+  samplePosts = [
+    { title: 'Learning Redux', author: testUser._id, tags: ['redux'] },
+    { title: 'Learn React Hooks', author: testUser._id, tags: ['react'] },
+    {
+      title: 'Full-Stack React Projects',
+      author: testUser._id,
+      tags: ['react', 'nodejs'],
+    },
+  ]
+})
 
 let createdSamplePosts = []
 
@@ -47,78 +54,90 @@ describe('getting a post', () => {
 
 describe('updating posts', () => {
   test('should update the specified property', async () => {
-    await updatePost(createdSamplePosts[0]._id, {
-      author: 'Test Author',
+    await updatePost(testUser._id, createdSamplePosts[0]._id, {
+      contents: 'some content change',
     })
     const updatedPost = await Post.findById(createdSamplePosts[0]._id)
-    expect(updatedPost.author).toEqual('Test Author')
+    expect(updatedPost.contents).toEqual('some content change')
   })
-
   test('should not update other properties', async () => {
-    await updatePost(createdSamplePosts[0]._id, {
-      author: 'Test Author',
+    await updatePost(testUser._id, createdSamplePosts[0]._id, {
+      contents: 'some content change',
     })
     const updatedPost = await Post.findById(createdSamplePosts[0]._id)
     expect(updatedPost.title).toEqual('Learning Redux')
   })
-
-  describe('deleting posts', () => {
-    test('should remove the post from the database', async () => {
-      const result = await deletePost(createdSamplePosts[0]._id)
-      expect(result.deletedCount).toEqual(1)
-      const deletedPost = await Post.findById(createdSamplePosts[0]._id)
-      expect(deletedPost).toEqual(null)
+  test('should update the updatedAt timestamp', async () => {
+    await updatePost(testUser._id, createdSamplePosts[0]._id, {
+      contents: 'some content change',
     })
-
-    test('should fail if the id does not exist', async () => {
-      const result = await deletePost('000000000000000000000000')
-      expect(result.deletedCount).toEqual(0)
+    const updatedPost = await Post.findById(createdSamplePosts[0]._id)
+    expect(updatedPost.updatedAt.getTime()).toBeGreaterThan(
+      createdSamplePosts[0].updatedAt.getTime(),
+    )
+  })
+  test('should fail if the id does not exist', async () => {
+    const post = await updatePost(testUser._id, '000000000000000000000000', {
+      contents: 'some content change',
     })
+    expect(post).toEqual(null)
+  })
+})
+
+describe('deleting posts', () => {
+  test('should remove the post from the database', async () => {
+    const result = await deletePost(testUser._id, createdSamplePosts[0]._id)
+    expect(result.deletedCount).toEqual(1)
+    const deletedPost = await Post.findById(createdSamplePosts[0]._id)
+    expect(deletedPost).toEqual(null)
   })
 
-  describe('creating posts', () => {
-    test('with all parameters should succeed', async () => {
-      const post = {
-        title: 'Hello Mongoose!',
-        author: 'Daniel Bugl',
-        contents: 'This post is stored in a MongoDB database using Mongoose.',
-        tags: ['mongoose', 'mongodb'],
-      }
-      const createdPost = await createPost(post)
-      expect(createdPost._id).toBeInstanceOf(mongoose.Types.ObjectId)
-      const foundPost = await Post.findById(createdPost._id)
-
-      expect(foundPost).toEqual(expect.objectContaining(post))
-      expect(foundPost.createdAt).toBeInstanceOf(Date)
-      expect(foundPost.updatedAt).toBeInstanceOf(Date)
-    })
-    test('without title should fail', async () => {
-      const post = {
-        author: 'Daniel Bugl',
-        contents: 'Post with no title',
-        tags: ['empty'],
-      }
-      try {
-        await createPost(post)
-      } catch (err) {
-        expect(err).toBeInstanceOf(mongoose.Error.ValidationError)
-        expect(err.message).toContain('`title` is required')
-      }
-    })
-    test('with minimal parameters should succeed', async () => {
-      const post = {
-        title: 'Only a title',
-      }
-      const createdPost = await createPost(post)
-      expect(createdPost._id).toBeInstanceOf(mongoose.Types.ObjectId)
-    })
+  test('should fail if the id does not exist', async () => {
+    const result = await deletePost('000000000000000000000000')
+    expect(result.deletedCount).toEqual(0)
   })
+})
 
-  describe('listing posts', () => {
-    test('should return all posts', async () => {
-      const posts = await listAllPosts()
-      expect(posts.length).toEqual(createdSamplePosts.length)
-    })
+describe('creating posts', () => {
+  test('with all parameters should succeed', async () => {
+    const post = {
+      title: 'Hello Mongoose!',
+      contents: 'This post is stored in a MongoDB database using Mongoose.',
+      tags: ['mongoose', 'mongodb'],
+    }
+    const createdPost = await createPost(testUser._id, post)
+    expect(createdPost._id).toBeInstanceOf(mongoose.Types.ObjectId)
+    const foundPost = await Post.findById(createdPost._id)
+
+    expect(foundPost).toEqual(expect.objectContaining(post))
+    expect(foundPost.createdAt).toBeInstanceOf(Date)
+    expect(foundPost.updatedAt).toBeInstanceOf(Date)
+  })
+  test('without title should fail', async () => {
+    const post = {
+      contents: 'Post with no title',
+      tags: ['empty'],
+    }
+    try {
+      await createPost(testUser._id, post)
+    } catch (err) {
+      expect(err).toBeInstanceOf(mongoose.Error.ValidationError)
+      expect(err.message).toContain('`title` is required')
+    }
+  })
+  test('with minimal parameters should succeed', async () => {
+    const post = {
+      title: 'Only a title',
+    }
+    const createdPost = await createPost(testUser._id, post)
+    expect(createdPost._id).toBeInstanceOf(mongoose.Types.ObjectId)
+  })
+})
+
+describe('listing posts', () => {
+  test('should return all posts', async () => {
+    const posts = await listAllPosts()
+    expect(posts.length).toEqual(createdSamplePosts.length)
   })
   test('should return posts sorted by creation date descending by default', async () => {
     const posts = await listAllPosts()
@@ -142,7 +161,7 @@ describe('updating posts', () => {
     )
   })
   test('should be able to filter posts by author', async () => {
-    const posts = await listPostsByAuthor('Daniel Bugl')
+    const posts = await listPostsByAuthor(testUser.username)
     expect(posts.length).toBe(3)
   })
   test('should be able to filter posts by tag', async () => {
